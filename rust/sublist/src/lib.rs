c5310348e6ee85446920ac69622671cd49e6e2c2
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-02-08 16:13:17 +0100

    cleaner solution by alireza4050

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,6 +7,19 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    use Comparison::*;
+
+    return match (first_list.len(), second_list.len()) {
+        (0, 0) => Equal,
+        (0, _) => Sublist,
+        (_, 0) => Superlist,
+        (l0, l1) if l0 > l1 => if first_list.windows(l1).any(|w| w == second_list) {Superlist} else {Unequal},
+        (l0, l1) if l0 < l1 => if second_list.windows(l0).any(|w| w == first_list) {Sublist} else {Unequal},
+        (_, _) => if first_list == second_list {Equal} else {Unequal},
+    };
+}
+
+pub fn _sublist_old<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     let (len0, len1) = (first_list.len(), second_list.len());
     if len0 == len1 {
         return if first_list == second_list {
