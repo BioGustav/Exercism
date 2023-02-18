3198746141b01f279c3d128b224924c395e0e42e
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-02-18 16:12:47 +0100

    enhance rust rpg

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -12,27 +12,29 @@ pub struct Player {
 impl Player {
     pub fn revive(&self) -> Option<Player> {
         return match (self.health, self.level) {
-            (0,l) if l >= REV_LVL_THRESHOLD => Some(Player { health: REV_HEALTH, mana: Some(REV_MANA), level: l }),
-            (0,l) => Some(Player { health: REV_HEALTH, mana: None, level: l }),
-            (_,_) => None,
-        }
+            (0, l) => Some(Player {
+                health: REV_HEALTH,
+                mana: (l >= REV_LVL_THRESHOLD).then(|| REV_MANA),
+                level: l,
+            }),
+            (_, _) => None,
+        };
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        return match (self.health, self.mana) {
-            (_,Some(mana)) if mana < mana_cost => 0,
-            (_,Some(mana)) => {
-                self.mana = Some(mana - mana_cost);
-                mana_cost * SPELL_MULTIPLIER
-            },
-            (health,_) if health >= mana_cost => {
-                self.health -= mana_cost;
-                0
-            },
-            (_,_) => {
-                self.health = 0;
+        return match self.mana {
+            Some(mana) => {
+                if mana < mana_cost {
+                    0
+                } else {
+                    self.mana = Some(mana - mana_cost);
+                    mana_cost * SPELL_MULTIPLIER
+                }
+            }
+            None => {
+                self.health = self.health.saturating_sub(mana_cost);
                 0
             }
-        }
+        };
     }
 }
