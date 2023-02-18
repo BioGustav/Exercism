7f8780e5b5114816a0c622ff9e57b7a6d8c317c8
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-02-18 13:24:03 +0100

    solved rust rpg

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -1,3 +1,8 @@
+const REV_HEALTH: u32 = 100;
+const REV_LVL_THRESHOLD: u32 = 10;
+const REV_MANA: u32 = 100;
+const SPELL_MULTIPLIER: u32 = 2;
+
 pub struct Player {
     pub health: u32,
     pub mana: Option<u32>,
@@ -6,10 +11,28 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        return match (self.health, self.level) {
+            (0,l) if l >= REV_LVL_THRESHOLD => Some(Player { health: REV_HEALTH, mana: Some(REV_MANA), level: l }),
+            (0,l) => Some(Player { health: REV_HEALTH, mana: None, level: l }),
+            (_,_) => None,
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {mana_cost}")
+        return match (self.health, self.mana) {
+            (_,Some(mana)) if mana < mana_cost => 0,
+            (_,Some(mana)) => {
+                self.mana = Some(mana - mana_cost);
+                mana_cost * SPELL_MULTIPLIER
+            },
+            (health,_) if health >= mana_cost => {
+                self.health -= mana_cost;
+                0
+            },
+            (_,_) => {
+                self.health = 0;
+                0
+            }
+        }
     }
 }
