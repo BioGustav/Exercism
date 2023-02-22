811da7dacbbf3dfd7d62a42664fe1d88b4283ad1
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-02-22 01:55:02 +0100

    refactor and optimize rust magazine

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,20 +1,13 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-//#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let mut mag_count: HashMap<&str, u32> = HashMap::new();
-    let mut note_count: HashMap<&str, u32> = HashMap::new();
+    let mut counter = magazine.iter().fold(HashMap::new(), |mut counter, word| {
+        *counter.entry(word).or_insert(0) += 1;
+        counter
+    });
 
-    for word in magazine {
-        mag_count.entry(*word).and_modify(|x| *x += 1).or_insert(1);
-    }
-    for word in note {
-        note_count.entry(*word).and_modify(|x| *x += 1).or_insert(1);
-    }
-    note_count
-        .iter()
-        .all(|(k, v)| mag_count.contains_key(*k) && mag_count[*k] >= *v)
+    note.iter().all(|word| {
+        *counter.entry(word).or_insert(0) -= 1;
+        counter[word] >= 0
+    })
 }
