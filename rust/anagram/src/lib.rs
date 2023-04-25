3f1054aa9775311805349a67f04aa02593b1f0b4
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-04-25 14:32:44 +0200

    it1 rust anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,7 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let mut matches = HashSet::new();
+
+    let mut w_sorted = word.chars().collect::<Vec<char>>();
+    w_sorted.sort();
+
+    for a in possible_anagrams {
+        let anagram = a.to_lowercase();
+        if anagram == word {
+            continue;
+        }
+        let mut a_sorted = anagram.chars().collect::<Vec<char>>();
+        a_sorted.sort();
+        if w_sorted == a_sorted {
+            matches.insert(a.to_owned());
+        }
+    }
+    matches
 }
