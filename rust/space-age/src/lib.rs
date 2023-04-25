ffb0981b0efee818664448d23ac6baa11e5a2eee
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-04-26 01:27:39 +0200

    fix rust space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -41,21 +41,3 @@ create_planet!(
     Uranus: 84.016846,
     Neptune: 164.79132
 );
-
-/* pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune; */
-
-/* impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {} */
