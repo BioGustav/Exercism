8bab58c34c86cdcfe9c0e0cb220cf0b0519e4cbd
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2023-05-09 16:42:16 +0200

    it1 rust luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,30 @@
+use std::str;
+
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {code} valid?");
+    if code.trim().len() <= 1 {
+        return false;
+    }
+
+    if code.bytes().any(|b| !(b.is_ascii_digit() || b.is_ascii_whitespace())) {
+        return false;
+    }
+
+    code.bytes()
+        .filter(|c| *c >= b'0')
+        .map(|c| (c - b'0') as u32)
+        .rev()
+        .enumerate()
+        .map(|(i, number)| match number {
+            number if i % 2 == 1 && number < 5 => number * 2,
+            number if i % 2 == 1 => number * 2 - 9,
+            _ => number,
+        })
+        .map(|e| {
+            dbg!(e);
+            e
+        })
+        .sum::<u32>()
+        % 10
+        == 0
 }
