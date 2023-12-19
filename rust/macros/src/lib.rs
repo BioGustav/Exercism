9d9c6cf7b378da93ec880a475302609e1bd46aa3
BioGustav | Tobias <pilz.tobias+github@gmail.com>
2023-12-19 23:13:06 +0100

    rust: solved macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,6 +1,11 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        todo!()
+    () => { crate::HashMap::new() };
+    ($($k:expr => $v:expr),+$(,)?) => {
+        {
+            let mut hm = crate::HashMap::new();
+            $(hm.insert($k, $v);)+
+            hm
+        }
     };
 }
